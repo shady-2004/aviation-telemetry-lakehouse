47e3b1b1b89fce03d5f39a7dd86ae579c51182bb
Shady mohamed <shadyshiko2004@gmail.com>
2026-08-28 15:21:27 +0300

    feat(silver): intermediate routes

---
+++ dbt/models/silver/intermediate/int_routes__enriched.sql
@@ -0,0 +1,62 @@
+with routes as (
+    select * from {{ ref('stg_postgres__routes') }}
+),
+
+airports as (
+    select * from {{ ref('stg_ourairports__airports') }}
+),
+
+joined as (
+    select
+        --  Deterministic Surrogate Key
+        {{ dbt_utils.generate_surrogate_key([
+            'r.airline_code',
+            'r.source_airport_code',
+            'r.destination_airport_code'
+        ]) }} as route_hk,
+
+        --  Route Identifiers
+        r.airline_code,
+        r.source_airport_code,
+        r.destination_airport_code,
+        r.equipment_codes,
+        r.stop_count,
+        r.is_codeshare,
+
+        --  Origin Airport Metadata
+        orig.airport_name as origin_airport_name,
+        orig.airport_type as origin_airport_type,
+        orig.iso_country as origin_country,
+        orig.latitude as origin_latitude,
+        orig.longitude as origin_longitude,
+        orig.elevation_ft as origin_elevation_ft,
+
+        --  Destination Airport Metadata
+        dest.airport_name as destination_airport_name,
+        dest.airport_type as destination_airport_type,
+        dest.iso_country as destination_country,
+        dest.latitude as destination_latitude,
+        dest.longitude as destination_longitude,
+        dest.elevation_ft as destination_elevation_ft,
+
+        --  Great-Circle Distance 
+        round(
+            2 * 6371.0 * asin(
+                sqrt(
+                    power(sin(radians(dest.latitude - orig.latitude) / 2), 2) +
+                    cos(radians(orig.latitude)) * cos(radians(dest.latitude)) *
+                    power(sin(radians(dest.longitude - orig.longitude) / 2), 2)
+                )
+            ), 2
+        ) as route_distance_km
+
+    from routes r
+    left join airports orig
+        on r.source_airport_code = orig.airport_ident
+        or r.source_airport_code = orig.iata_code
+    left join airports dest
+        on r.destination_airport_code = dest.airport_ident
+        or r.destination_airport_code = dest.iata_code
+)
+
+select * from joined
