cc8c045e8c3d7e9aa08bfd2420a7512a3dda572c
Shady mohamed <shadyshiko2004@gmail.com>
2026-09-01 18:11:39 +0300

    fix: data issues & pipeline configs

--- dbt/models/silver/intermediate/int_routes__enriched.sql
+++ dbt/models/silver/intermediate/int_routes__enriched.sql
@@ -6,16 +6,43 @@ airports as (
     select * from {{ ref('stg_ourairports__airports') }}
 ),
 
+--  Build a strict 1:1 lookup mapping for each airport code
+airport_lookup as (
+    select
+        coalesce(iata_code, airport_ident) as lookup_code,
+        airport_name,
+        airport_type,
+        iso_country,
+        latitude,
+        longitude,
+        elevation_ft
+    from airports
+    where coalesce(iata_code, airport_ident) is not null
+    -- Prioritize operational and large airports in case of duplicate code collisions
+    qualify row_number() over (
+        partition by coalesce(iata_code, airport_ident)
+        order by 
+            case airport_type
+                when 'large_airport' then 1
+                when 'medium_airport' then 2
+                when 'small_airport' then 3
+                else 4
+            end,
+            scheduled_service desc
+    ) = 1
+),
+
+-- 
 joined as (
     select
-        --  Deterministic Surrogate Key
+        -- Deterministic Surrogate Key
         {{ dbt_utils.generate_surrogate_key([
             'r.airline_code',
             'r.source_airport_code',
             'r.destination_airport_code'
         ]) }} as route_hk,
 
-        --  Route Identifiers
+        -- Route Identifiers
         r.airline_code,
         r.source_airport_code,
         r.destination_airport_code,
@@ -23,7 +50,7 @@ joined as (
         r.stop_count,
         r.is_codeshare,
 
-        --  Origin Airport Metadata
+        -- Origin Airport Metadata
         orig.airport_name as origin_airport_name,
         orig.airport_type as origin_airport_type,
         orig.iso_country as origin_country,
@@ -31,7 +58,7 @@ joined as (
         orig.longitude as origin_longitude,
         orig.elevation_ft as origin_elevation_ft,
 
-        --  Destination Airport Metadata
+        -- Destination Airport Metadata
         dest.airport_name as destination_airport_name,
         dest.airport_type as destination_airport_type,
         dest.iso_country as destination_country,
@@ -39,24 +66,26 @@ joined as (
         dest.longitude as destination_longitude,
         dest.elevation_ft as destination_elevation_ft,
 
-        --  Great-Circle Distance 
-        round(
-            2 * 6371.0 * asin(
-                sqrt(
-                    power(sin(radians(dest.latitude - orig.latitude) / 2), 2) +
-                    cos(radians(orig.latitude)) * cos(radians(dest.latitude)) *
-                    power(sin(radians(dest.longitude - orig.longitude) / 2), 2)
-                )
-            ), 2
-        ) as route_distance_km
+        -- Great-Circle Distance (Safe from NULLs)
+        case 
+            when orig.latitude is not null and dest.latitude is not null
+            then round(
+                2 * 6371.0 * asin(
+                    sqrt(
+                        power(sin(radians(dest.latitude - orig.latitude) / 2), 2) +
+                        cos(radians(orig.latitude)) * cos(radians(dest.latitude)) *
+                        power(sin(radians(dest.longitude - orig.longitude) / 2), 2)
+                    )
+                ), 2
+            )
+            else null
+        end as route_distance_km
 
     from routes r
-    left join airports orig
-        on r.source_airport_code = orig.airport_ident
-        or r.source_airport_code = orig.iata_code
-    left join airports dest
-        on r.destination_airport_code = dest.airport_ident
-        or r.destination_airport_code = dest.iata_code
+    left join airport_lookup orig
+        on r.source_airport_code = orig.lookup_code
+    left join airport_lookup dest
+        on r.destination_airport_code = dest.lookup_code
 )
 
 select * from joined
--- src/utils/minio_client.py
+++ src/utils/minio_client.py
@@ -7,7 +7,7 @@ load_dotenv()
 # -------------------------------------------------------------------
 # Configuration
 # -------------------------------------------------------------------
-MINIO_ENDPOINT = os.environ.get("MINIO_ENDPOINT", "127.0.0.1:9000")
+MINIO_ENDPOINT = os.environ.get("MINIO_ENDPOINT", "minio:9000")
 MINIO_ACCESS_KEY = os.environ.get("MINIO_ACCESS_KEY", "minioadmin")
 MINIO_SECRET_KEY = os.environ.get("MINIO_SECRET_KEY", "minioadmin")
 MINIO_SECURE = os.environ.get("MINIO_SECURE", "false").lower() in ("true", "1", "yes")
